eda20197a9e4480ddb7a357f39adfdc6c9ad6dd0
Daniel Montero <daniel.montero@simiolabs.com>
2017-04-18 12:56:40 -0600

    Add read and write rates functions.

--- utilities_calc_cr/downloader.py
+++ utilities_calc_cr/downloader.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import re
 
 def download_rates(urls, dest_dir):
     if not os.path.exists(dest_dir):
@@ -11,3 +12,20 @@ def download_rates(urls, dest_dir):
         print 'Retrieving...', url,
         urllib.urlretrieve(url, os.path.join(dest_dir, local_name))
         i += 1
+
+def read_rates(dir):
+    f = open(dir + '/page0.html', 'r')
+    text = f.read()
+    f.close()
+    matches = re.findall(r'(\S+,\d+)</p>', text)
+    #TODO dirty fix for html page error
+    if matches:
+        matches[6] = '180,78'
+        print matches
+    return matches
+
+def save_rates(rates, dest_dir, filename):
+    f = open(dest_dir + '/' + filename, 'w')
+    for rate in rates:
+        f.write(rate + '\n')
+    f.close()
