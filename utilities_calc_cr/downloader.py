5e6ad47d361d49f8ff6c94d3a44010011a96b5ed
Daniel Montero <daniel.montero@simiolabs.com>
2017-04-25 13:46:20 -0600

    Nice new function to download last month's log.

--- utilities_calc_cr/downloader.py
+++ utilities_calc_cr/downloader.py
@@ -1,6 +1,9 @@
 import os
 import urllib
 import re
+import datetime
+
+LOG_NAME = 'log'
 
 def download_rates(urls, dest_dir):
     if not os.path.exists(dest_dir):
@@ -75,4 +78,26 @@ def download_old_log(dest_dir):
 
     local_name = 'log'
     print 'Retrieving...'
-    urllib.urlretrieve('http://data.sparkfun.com/output/JxKdMWGdMViN2784OQb1.json?gte[timestamp]=04-01-2017&eq[name]=total', os.path.join(dest_dir, local_name))
+    urllib.urlretrieve('http://data.sparkfun.com/output/JxKdMWGdMViN2784OQb1.json?gte[timestamp]=03-01-2017&lt[timestamp]=04-01-2017&eq[name]=total', os.path.join(dest_dir, local_name))
+
+def get_last_month_log(dest_dir):
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    this_month = datetime.date.today()
+    if this_month.month is 1:
+        this_month = this_month.replace(day=1)
+        last_month = this_month.replace(day=1, month=12)
+    else:
+        this_month = this_month.replace(day=1)
+        last_month = this_month.replace(day=1, month=this_month.month - 1)
+    end_month = this_month.strftime('%m-%d-%Y')
+    start_month = last_month.strftime('%m-%d-%Y')
+
+    local_name = LOG_NAME
+    url = 'http://data.sparkfun.com/output/JxKdMWGdMViN2784OQb1.json?gte[timestamp]='+start_month+'&lt[timestamp]='+end_month+'&eq[name]=total'
+    print 'Retrieving...'
+    try:
+        urllib.urlretrieve(url, os.path.join(dest_dir, local_name))
+    except Exception, e:
+        print e
