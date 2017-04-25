b9ebeff4fe131883f9dd83fdc7813ecf31939d93
Daniel Montero <daniel.montero@simiolabs.com>
2017-04-25 13:57:51 -0600

    Fix some dumb typos.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -28,3 +28,15 @@ def test_get_consumption_segment_pr():
     assert_equal(get_consumption_segment_pr(5, PR_POWER), PR_P_LOW)
     assert_equal(get_consumption_segment_pr(200, PR_POWER), PR_MID)
     assert_equal(get_consumption_segment_pr(5000, PR_POWER), PR_P_HIGH)
+
+def test_get_fire_department_tribute():
+    assert_equal(get_fire_department_tribute(0, 1567.66, 'TRR'), 117.5745)
+    assert_equal(get_fire_department_tribute(0, 1200, 'RR'), 90.0)
+    assert_equal(get_fire_department_tribute(0, 5937.21, 'PR'), 445.29075)
+
+    assert_equal(get_fire_department_tribute(10, 1567.66, 'NA'), 20.5755375)
+    assert_equal(get_fire_department_tribute(20, 1200, 'NA'), 7.875)
+    assert_equal(get_fire_department_tribute(30, 9713.21, 'NA'), 42.49529375)
+
+def test_get_street_lighting_tribute():
+    assert_equal(get_street_lighting_tribute(450.87), 1582.5537)
