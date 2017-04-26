267bcefdfc79f97c65f4f732a3a9223b696184a7
Daniel Montero <daniel.montero@simiolabs.com>
2017-04-26 14:26:06 -0600

    Change directory name.

--- utilities_calc_cr/parser.py
+++ utilities_calc_cr/parser.py
@@ -1,9 +1,9 @@
 import os
 import re
 
-def read_rates(dir):
-    if os.path.exists(dir + '/page0.html'):
-        f = open(dir + '/page0.html', 'r')
+def read_rates(dirname):
+    if os.path.exists(dirname + '/page0.html'):
+        f = open(dirname + '/page0.html', 'r')
         text = f.read()
         f.close()
         matches = re.findall(r'(\S+,\d+)</p>', text)
@@ -21,10 +21,10 @@ def save_rates(rates, dest_dir, filename):
         f.write(rate + '\n')
     f.close()
 
-def get_trr(dir, filename):
-    if os.path.exists(dir + '/' + filename):
+def get_trr(dirname, filename):
+    if os.path.exists(dirname + '/' + filename):
         rate_list = []
-        f = open(dir + '/' + filename, 'r')
+        f = open(dirname + '/' + filename, 'r')
         rate_list = f.read().splitlines()
 
         i = 0
@@ -39,38 +39,41 @@ def get_trr(dir, filename):
         print twod_list
         return twod_list
 
-def get_rr(dir, filename):
-    if os.path.exists(dir + '/' + filename):
+def get_rr(dirname, filename):
+    if os.path.exists(dirname + '/' + filename):
         rate_list = []
-        f = open(dir + '/' + filename, 'r')
+        f = open(dirname + '/' + filename, 'r')
         rate_list = f.read().splitlines()
         rate_list = rate_list[9:13]
         print rate_list
         return rate_list
 
-def get_pr(dir, filename):
-    if os.path.exists(dir + '/' + filename):
+def get_pr(dirname, filename):
+    if os.path.exists(dirname + '/' + filename):
         rate_list = []
-        f = open(dir + '/' + filename, 'r')
+        f = open(dirname + '/' + filename, 'r')
         rate_list = f.read().splitlines()
         rate_list = rate_list[13:]
         print rate_list
         return rate_list
 
-def log_to_days(dir, filename):
-    if os.path.exists(dir + '/' + filename):
-        log_file = open(dir + '/' + filename, 'r')
+def log_to_days(dirname, filename):
+    if os.path.exists(dirname + '/' + filename):
+        log_file = open(dirname + '/' + filename, 'r')
         for line in log_file:
             match = re.search(r'(\d+-\d+-\d+)', line)
             if match:
                 month_log_name = match.group(1)[:7]
-                if not os.path.exists(dir + '/' + month_log_name):
-                    os.makedirs(dir + '/' + month_log_name)
+                if not os.path.exists(dirname + '/' + month_log_name):
+                    os.makedirs(dirname + '/' + month_log_name)
                 day_log_name = match.group(1)
-                day_log_file = open(dir + '/' + month_log_name + '/' + \
+                day_log_file = open(dirname + '/' + month_log_name + '/' + \
                                     day_log_name, 'a')
                 day_log_file.write(line)
                 day_log_file.close()
         log_file.close()
 
-#def days_to_
+def days_to_segments(dirname):
+    if os.path.exists(dirname):
+        paths = os.listdir(dirname)
+        print paths
