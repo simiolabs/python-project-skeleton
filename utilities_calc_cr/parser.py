3b408b575c7ffdfcf45a935ee172d07320bccb91
Daniel Montero <daniel.montero@simiolabs.com>
2017-05-03 14:57:00 -0600

    Convert from kW to kWh is now complete.

--- utilities_calc_cr/parser.py
+++ utilities_calc_cr/parser.py
@@ -147,8 +147,8 @@ def convert_w_to_wh(data_dic):
         s1 = timedelta(hours=t1.tm_hour,minutes=t1.tm_min,seconds=t1.tm_sec).total_seconds() # convert t1 to secs
         s2 = timedelta(hours=t2.tm_hour,minutes=t2.tm_min,seconds=t2.tm_sec).total_seconds() # convert t2 to secs
         t_delta = s2 - s1
-        wh = float(data_dic[sorted_keys[i]]) * t_delta / 3600
-        temp_list.append(data_dic[sorted_keys[i]])
+        wh = float(data_dic[sorted_keys[i]][0]) * t_delta / 3600
+        temp_list.append(data_dic[sorted_keys[i]][0])
         temp_list.append(str(t_delta))
         temp_list.append(str(wh))
         data_dic[sorted_keys[i]] = temp_list
@@ -156,12 +156,20 @@ def convert_w_to_wh(data_dic):
     return data_dic
 
 def get_watts_hour(dirname):
-    """Take day long log file, extract timestamp and real power and save it."""
+    """
+    Take all the files ending with .kw in a folder and, extract the timestamp
+    and kW and calculate kW. Then save all the data like:
+      timestamp power timedelta energy
+      HH:MM:SS  kW    s         kWh
+    """
     if os.path.exists(dirname):
         paths = os.listdir(dirname)
         paths = sorted(paths)
         for path in paths:
             if path.endswith(KW_LOG):
+                print 'Reading...', path
                 data_dic = load_dic_from_file(dirname, path)
                 new_data_dic = convert_w_to_wh(data_dic)
-                save_dic_to_file(dirname, path[:-3] + KWH_LOG, new_data_dic)
+                new_file_name = path[:-3] + KWH_LOG
+                print 'Saving...', new_file_name
+                save_dic_to_file(dirname, new_file_name, new_data_dic)
