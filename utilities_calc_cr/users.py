b262834637d9ae8c290661a291bbd42ad090a1f7
Daniel Montero <daniel.montero@simiolabs.com>
2017-05-05 15:08:21 -0600

    Save db and pk info.

--- utilities_calc_cr/users.py
+++ utilities_calc_cr/users.py
@@ -3,6 +3,7 @@ import os
 utilities = [ 'electricity', 'water' ]
 electricity_companies = [ 'cnfl' ]
 water_companies = [ 'aya' ]
+db = [ 'phant' ]
 
 USER_INFO = 'info'
 LOG_DIR = 'logs'
@@ -11,7 +12,11 @@ class User(object):
     def __init__(self, user_id):
         self.user_id = user_id
         self.electricity_company = ''
+        self.electricity_db = ''
+        self.electricity_pk = ''
         self.water_company = ''
+        self.water_db = ''
+        self.water_pk = ''
 
     def assign_company(self, utility, company):
         if (utility is utilities[0] and company in electricity_companies):
@@ -21,17 +26,36 @@ class User(object):
         else:
             print 'Undefined %s company: %s' % (utility, company)
 
+    def assign_electric_db(self, database, public_key):
+        if database in db and public_key:
+            self.electricity_db = database
+            self.electricity_pk = public_key
+
+    def assign_water_db(self, database, public_key):
+        if database in db and public_key:
+            self.water_db = database
+            self.water_pk = public_key
+
     def print_info(self):
-        print 'User: %s' % (self.user_id)
-        print 'Electricity: %s' % (self.electricity_company)
-        print 'Water: %s' % (self.water_company)
+        print 'User:', self.user_id
+        print 'Electricity company:', self.electricity_company
+        print 'Electricity DB:', self.electricity_db
+        print 'Electricity PK:', self.electricity_pk
+        print 'Water company:', self.water_company
+        print 'Water DB:', self.water_db
+        print 'Water PK:', self.water_pk
 
     def create_account(self):
         if not os.path.exists(self.user_id):
             os.makedirs(self.user_id)
 
-        f = open(self.user_id + '/' + USER_INFO, 'a')
+        f = open(self.user_id + '/' + USER_INFO, 'w')
         f.write(self.user_id+ '\n')
-        f.write(self.electricity_company+ '\n')
-        f.write(self.water_company)
+        f.write(self.electricity_company + self.electricity_db + \
+                self.electricity_pk + '\n')
+        f.write(self.water_company + self.water_db + self.water_pk + '\n')
         f.close()
+
+#    def get_last_month_log(database):
+#        if database == db[0]:
+#            downloader.get_last_month_log(public_key, dest_dir)
