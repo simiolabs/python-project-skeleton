2261bae4cba808d56d6c3f1f18687a2ec7f9e0de
Daniel Montero <daniel.montero@simiolabs.com>
2017-05-17 13:37:44 -0600

    Style: dirty typo

--- tests/test_electric.py
+++ tests/test_electric.py
@@ -5,7 +5,7 @@ def test_get_time_segment():
     assert_equal(get_time_segment('07:00:00'), OFF_PEAK_TIME)
     assert_equal(get_time_segment('11:00:00'), PEAK_TIME)
     assert_equal(get_time_segment('12:00:00'), PEAK_TIME)
-    assert_equal(get_time_segment('15:00:00)'), OFF_PEAK_TIME)
+    assert_equal(get_time_segment('15:00:00'), OFF_PEAK_TIME)
     assert_equal(get_time_segment('18:00:00'), PEAK_TIME)
     assert_equal(get_time_segment('21:00:00'), NIGHT_TIME)
     assert_equal(get_time_segment('02:00:00'), NIGHT_TIME)
