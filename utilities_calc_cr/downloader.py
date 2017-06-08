76517ad1a919641d10a826895b338920b8ce9fb3
Daniel Montero <daniel.montero@simiolabs.com>
2017-06-08 14:52:15 -0600

    Feat: completely recode download functions
    Now this will work with both Phant.io and EmonCMS. Last month's log is
    downloaded by default but can be used for every time frame.

--- utilities_calc_cr/downloader.py
+++ utilities_calc_cr/downloader.py
@@ -28,11 +28,8 @@ def download_rates(filename, dest_dir):
         except Exception, e:
             print e
 
-def get_last_month_log(public_key, dest_dir):
-    """Download last's month log from Phant.io and save it."""
-    if not os.path.exists(dest_dir):
-        os.makedirs(dest_dir)
-
+def get_last_month_log(public_key, dest_dir, database):
+    """Download last's month log and save it to a file."""
     this_month = datetime.date.today()
     if this_month.month is 1:
         this_month = this_month.replace(day=1)
@@ -40,12 +37,22 @@ def get_last_month_log(public_key, dest_dir):
     else:
         this_month = this_month.replace(day=1)
         last_month = this_month.replace(day=1, month=this_month.month - 1)
-    end_month = this_month.strftime('%m-%d-%Y')
-    start_month = last_month.strftime('%m-%d-%Y')
 
-    local_name = last_month.strftime('%Y-%m' + LOG)
+    if database == 'phant':
+        get_log_phant(public_key, dest_dir, last_month, this_month)
+    elif database == 'emoncms':
+        get_log_emoncms(public_key, dest_dir, last_month, this_month)
+
+def get_log_phant(public_key, dest_dir, start_date, end_date):
+    """Download log from Phant.io and save it to a file."""
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    end = end_date.strftime('%m-%d-%Y')
+    start = start_date.strftime('%m-%d-%Y')
+    local_name = start_date.strftime('%Y-%m' + LOG)
     #FIXME: name is hardcoded
-    url = 'http://data.sparkfun.com/output/'+public_key+'.json?gte[timestamp]='+start_month+'&lt[timestamp]='+end_month+'&eq[name]=total'
+    url = 'http://data.sparkfun.com/output/'+public_key+'.json?gte[timestamp]='+start+'&lt[timestamp]='+end+'&eq[name]=total'
     print 'Retrieving...', url
     try:
         urllib.urlretrieve(url, os.path.join(dest_dir, local_name))
@@ -53,32 +60,22 @@ def get_last_month_log(public_key, dest_dir):
     except Exception, e:
         print e
 
-def get_last_month_log_emoncms(public_key, dest_dir):
-    """Download last's month log from EmonCMS and save it."""
+def get_log_emoncms(public_key, dest_dir, start_date, end_date):
+    """Download log from EmonCMS.org and save it to a file."""
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
-    this_month = datetime.date.today()
-    if this_month.month is 1:
-        this_month = this_month.replace(day=1)
-        last_month = this_month.replace(day=1, month=12)
-    else:
-        this_month = this_month.replace(day=1)
-        last_month = this_month.replace(day=1, month=this_month.month - 1)
-    end_month = this_month.strftime('%m-%d-%Y')
-    start_month = last_month.strftime('%m-%d-%Y')
-    end_ts = str(int(time.mktime(this_month.timetuple())) * 1000)
-    start_ts = str(int(time.mktime(last_month.timetuple())) * 1000)
-
-    local_name = last_month.strftime('%Y-%m' + LOG)
+    end_ts = str(int(time.mktime(end_date.timetuple())) * 1000)
+    start_ts = str(int(time.mktime(start_date.timetuple())) * 1000)
+    local_name = start_date.strftime('%Y-%m' + LOG)
     #FIXME: node is hardcoded
     url = 'http://simiolabs.com/emoncms/feed/data.json?id=1&apikey='+public_key+'&start='+start_ts+'&end='+end_ts+'&interval=300'
     user_agent = 'Utilities Calc CR'
     values = { }
     headers = {'User-Agent': user_agent}
-
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data, headers)
+    print 'Retrieving...', url
     try:
         response = urllib2.urlopen(req)
         f = open(dest_dir + '/' + local_name, 'w')
